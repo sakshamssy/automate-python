f936fbc296deee2969b5be2fb16d9eca86595f44
Saksham Yadav <ssysaksham@gmail.com>
2023-09-28 12:38:16 +0530

    Weather data scrapped

--- BrowserAutomation/TextScrapping.py
+++ BrowserAutomation/TextScrapping.py
@@ -7,16 +7,16 @@ def get_driver():
     options.add_argument("start-maximized")  #webpages remove info in minimised option
     options.add_argument("disable-dev-shm-usage") #avoid linux se scraping problems
     options.add_argument("no-sandbox") #for greater accessebility
-    options.add_experimental_options("excludeSwitches",["enable-automation"])
+    options.add_experimental_option("excludeSwitches",["enable-automation"])
     options.add_argument("disable-blink-features=AutomationControlled")
 
     driver = webdriver.Chrome(options=options)
-    driver.get("https://arcbphc.vercel.app/")
+    driver.get("https://www.timeanddate.com/weather/india/hyderabad/ext")
     return driver
 
 def main():
     driver = get_driver()
-    element = driver.find_element(by="xpath",value ="/html/body/div/div[2]/div[2]/span[2]")
-    return element
+    element = driver.find_element(by="xpath",value ="/html/body/div[5]/main/article/div[1]/div")
+    return element.text
 
 print(main())
